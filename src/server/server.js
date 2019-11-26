7662c73d2a29396e0e2c9fef41b758f6ffcddab6
Saron <saronnhong@gmail.com>
2019-11-26 14:56:47 -0800

    display correctly of title and all headers, add error for no title or headers found

--- src/server/server.js
+++ src/server/server.js
@@ -17,7 +17,6 @@ app.post("/findfriends", async (req, res, next) => {
   try {
     const friends = await findFriends(website);
     res.send(friends);
-
   } catch (err) {
     console.error("oops", err)
     res.status(500).send({
@@ -35,41 +34,37 @@ app.listen(port, () => {
 
 /** TODO return promise of friends json. */
 function findFriends(website) {
-  
   return axios.get(website).then(function (res) {
     var results = {};
-    
+    results.title = "";
+    results.headers = [];
     const $ = cheerio.load(res.data);
 
     $("head").each(function (i, element) {
       results.title = $(this).find("title").text().trim();
-      console.log("Title: ", results.title);
     });
 
-    $("h3").each(function (i, element) {
-      results.headers=[];
-      var foundHeaders = $(this).text().trim();
-      if(foundHeaders.includes("kitten") || foundHeaders.includes("dog") || foundHeaders.includes("puppy")){
+    $(":header").each(function (i, element) {
+      var foundHeaders = $(this).text().trim().toLowerCase();
+      if (foundHeaders.includes("cat") || foundHeaders.includes("kitten") || foundHeaders.includes("dog") || foundHeaders.includes("puppy")) {
+        foundHeaders = foundHeaders[0].toUpperCase() + foundHeaders.slice(1);     //Capitalizes first letter in Sentence
         results.headers.push(foundHeaders);
-      }
+      };
     });
-    
-    console.log("Headers: ",results.headers);
-    
-    // if ((results.headers.length === 0)){
-    //   throw new Error("I don't know how to find friends!");
-    // }else{
-    //   return results;
-    // }
-    return results;
+
+    if ((results.headers.length === 0) && (results.title === "")) {
+      throw new Error("I don't know how to find friends!");
+    } else {
+      return results;
+    };
   });
-    
 
-  
-  
-  
 
-  
-  
+
+
+
+
+
+
 
 }
